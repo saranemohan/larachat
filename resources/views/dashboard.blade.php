58b9f610c53ce07fa17e8ea0f48791bd2ae21cdd
saran <saranemohan91@gmail.com>
2021-01-16 17:49:05 +0530

    Chat UI/UX Fixes #5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,32 @@
             <div class="col-start-2 col-span-3">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+                        <div class="border-b-2 border-fuchsia-600 pb-3 mb-4">
+                            <h1 class="text-xl">Aswina</h1>
+                            <p class="text-gray-400">aswina@techfriar.com</p>
+                        </div>
+                        <div>
+                            <x-chat-message class="justify-self-end">
+                                How's it ?
+                            </x-chat-message>
+                            <x-chat-message class="justify-self-start">
+                                Nice Work!
+                            </x-chat-message>
+                            <x-chat-message class="justify-self-end">
+                                Thank You
+                            </x-chat-message>
+                            <x-chat-message class="justify-self-start">
+                                Welcome
+                            </x-chat-message>
+                        </div>
+                        <div class="grid grid-cols-6 gap-4 mt-4">
+                            <div class="col-span-5">
+                                <x-input id="message" class="w-full" type="text" name="message" placeholder="Message" :value="old('message')" autofocus />
+                            </div>
+                            <div class="">
+                                <x-button class="w-full justify-center"> {{ __('Send') }} </x-button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
