90c9b5d38d52b4a60cfa188bbafdf6cf35cef469
Saran E mohan <30310459+saranemohan@users.noreply.github.com>
2021-01-16 14:31:52 +0530

    saranemohan/issue4 (#8)
    * User List after login
    Fixes #4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Laravel Chat') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+    <div class="py-6">
+
+        <div class="grid grid-cols-4 gap-4">
+            <div class="col-start-1 col-span-1">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="grid grid-flow-row auto-rows-max gap-1">
+                            @foreach ($users as $user)
+                            <x-list-item class="ml-3">
+                                {{ $user->name }}
+                            </x-list-item>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-start-2 col-span-3">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        You're logged in!
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'] )
+        ->middleware(['auth'])
+        ->name('dashboard');
 
 require __DIR__.'/auth.php';
